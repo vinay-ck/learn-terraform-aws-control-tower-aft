1bd7cff6ed88dc647557554fa86d5ff5ec529a24
vinay-ck <vinay.naidu@cloud-kinetics.com>
2023-06-14 19:40:42 +0800

    add versions.tf

--- terraform.tfvars
+++ terraform.tfvars
@@ -8,3 +8,5 @@ aft_management_account_id   = "318044964294"
 github_username             = "vinay-ck"
 ct_home_region              = "ap-southeast-1"
 tf_backend_secondary_region = "ap-southeast-2"
+aws_region                  = "ap-southeast-1"
+
---
+++ versions.tf
@@ -0,0 +1,17 @@
+Terraform {
+  cloud {
+    organization = "CloudKineticsTechPteLtd"
+    workspaces {
+      name = "terraform-aws-control-tower-aft-deployment"
+    }
+  }
+  required_providers {
+	aws = {
+  	source  = "hashicorp/aws"
+  	version = ">= 4.27.0, < 5.0.0"
+	}
+  }
+  provider "aws" {
+    region = var.aws_region
+    }
+}
